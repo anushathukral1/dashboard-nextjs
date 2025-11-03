148d279a7b6a393f6f330915261620fcff77523a
anushathukral1 <anusha@kindcause.org>
2025-11-03 00:41:30 +0000

    added files

---
+++ ssr-dashboard/app/dashboard/page.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>✅ Dashboard Page Loaded</h1>
+      <p>If you see this, routing works!</p>
+    </div>
+  );
+}
